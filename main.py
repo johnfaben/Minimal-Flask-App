ce3a322d56e8dd25989fd67e9e18aa7381585b7d
John Faben <jdfaben@gmail.com>
2021-02-25 00:31:31 +0000

    Moved all logic fully over to MongoDB. Added intermediate login page with Google Login button.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import app
 
-from make_hipes import add_hipes
-add_hipes()
+#from make_hipes import add_hipes
+#add_hipes()
 app.run(
       host = "0.0.0.0", # or 127.0.0.1 (DONT USE LOCALHOST)
       port = 8080,
